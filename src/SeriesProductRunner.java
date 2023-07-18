ef2321c69be213f2604685068c1f45242bdf9bff
majnumiah1588 <136418213+majnumiah1588@users.noreply.github.com>
2023-07-18 22:48:59 +0600

    Delete src directory

--- src/SeriesProductRunner.java
+++
@@ -1,8 +0,0 @@
-public class SeriesProductRunner {
-    public static void main(String [] args){
-
-        SeriesProduct seriesProduct = new SeriesProduct();
-        
-        System.out.println(seriesProduct.SeriesProduct(10));
-    }
-}
